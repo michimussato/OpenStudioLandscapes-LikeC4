7fa56a759def88698d5dad89e90cd6fc6dd8d568
Michael Mussato <michimussato@gmail.com>
2025-11-17 11:11:44 +0100

    44-update-documentation: remove overrides in docker compose image paths

--- src/OpenStudioLandscapes/LikeC4/assets.py
+++ src/OpenStudioLandscapes/LikeC4/assets.py
@@ -425,8 +425,9 @@ def compose_likec4(
                 "hostname": host_name,
                 "domainname": env["OPENSTUDIOLANDSCAPES__DOMAIN_LAN"],
                 "restart": "always",
-                "image": "${DOT_OVERRIDES_REGISTRY_NAMESPACE:-docker.io/openstudiolandscapes}/%s:%s"
-                % (build["image_name"], build["image_tags"][0]),
+                # "image": "${DOT_OVERRIDES_REGISTRY_NAMESPACE:-docker.io/openstudiolandscapes}/%s:%s"
+                # % (build["image_name"], build["image_tags"][0]),
+                "image": "%s:%s" % (build["image_name"], build["image_tags"][0]),
                 **copy.deepcopy(network_dict),
                 **copy.deepcopy(volumes_dict),
                 "command": [
