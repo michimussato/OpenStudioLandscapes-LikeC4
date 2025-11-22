e901aefed8e9d61c9e511bbfa5c56794ecd6ab37
Michael Mussato <michimussato@gmail.com>
2025-11-22 10:32:23 +0100

    48-greyed-out-create-resource-button-in-pangolin: container_name max 63 chars per segment 48-greyed-out-create-resource-button-in-pangolin: remove backoff - use Dagsters backoff (RetryPolicy)

--- src/OpenStudioLandscapes/LikeC4/assets.py
+++ src/OpenStudioLandscapes/LikeC4/assets.py
@@ -415,10 +415,16 @@ def compose_likec4(
     }
 
     service_name = "likec4"
-    container_name = "--".join([service_name, env.get("LANDSCAPE", "default")])
-    host_name = ".".join(
-        [env["HOSTNAME"] or service_name, env["OPENSTUDIOLANDSCAPES__DOMAIN_LAN"]]
+    container_name, host_name = get_docker_compose_names(
+        context=context,
+        service_name=service_name,
+        landscape_id=env.get("LANDSCAPE", "default"),
+        domain_lan=env.get("OPENSTUDIOLANDSCAPES__DOMAIN_LAN"),
     )
+    # container_name = "--".join([service_name, env.get("LANDSCAPE", "default")])
+    # host_name = ".".join(
+    #     [service_name, env["OPENSTUDIOLANDSCAPES__DOMAIN_LAN"]]
+    # )
 
     docker_dict = {
         # "networks": compose_networks.get("networks", []),
