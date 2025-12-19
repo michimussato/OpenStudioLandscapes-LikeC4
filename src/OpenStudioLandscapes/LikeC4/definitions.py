e7f5ef21fcf8b9f7bfe977127f3bbb281a54c1a9
Michael Mussato <michimussato@gmail.com>
2025-12-19 15:01:34 +0100

    47-refactor-to-config-based-landscapes: OpenStudioLandscapes-OpenCue refactored

--- src/OpenStudioLandscapes/LikeC4/definitions.py
+++ src/OpenStudioLandscapes/LikeC4/definitions.py
@@ -4,20 +4,14 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.LikeC4.assets
-import OpenStudioLandscapes.LikeC4.constants
 
 assets = load_assets_from_modules(
     modules=[OpenStudioLandscapes.LikeC4.assets],
-)
-
-constants = load_assets_from_modules(
-    modules=[OpenStudioLandscapes.LikeC4.constants],
 )
 
 
 defs = Definitions(
     assets=[
         *assets,
-        *constants,
     ],
 )
