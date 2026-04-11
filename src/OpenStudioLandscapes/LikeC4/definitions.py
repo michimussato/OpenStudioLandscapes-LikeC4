86a5580ff26a53c6b2716c76c8b0541b4b7d2e76
Michael Mussato <michimussato@gmail.com>
2026-04-11 13:30:24 +0200

    implements individual development/testing/debugging functionality

--- src/OpenStudioLandscapes/LikeC4/definitions.py
+++ src/OpenStudioLandscapes/LikeC4/definitions.py
@@ -4,16 +4,14 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.LikeC4.assets
-from OpenStudioLandscapes.engine.features.upstream_asset_specs import assets_external
 
-assets = load_assets_from_modules(
+assets_base = load_assets_from_modules(
     modules=[OpenStudioLandscapes.LikeC4.assets],
 )
 
 
 defs = Definitions(
     assets=[
-        *assets,
-        *assets_external,
+        *assets_base,
     ],
 )
