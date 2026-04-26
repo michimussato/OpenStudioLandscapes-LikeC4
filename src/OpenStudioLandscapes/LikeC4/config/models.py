69e65428859aa03208428a2e49119095bc3c68b8
Michael Mussato <michimussato@gmail.com>
2026-04-26 17:52:02 +0200

    add computed_field to properties

--- src/OpenStudioLandscapes/LikeC4/config/models.py
+++ src/OpenStudioLandscapes/LikeC4/config/models.py
@@ -6,6 +6,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
+    computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -89,6 +90,7 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
+    @computed_field
     @property
     def likec4_DATA_ROOT_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
